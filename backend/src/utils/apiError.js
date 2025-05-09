624fc27c0b9bfa39d16c3271da91ac643b287329
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-05-09 13:10:17 +0530

    fix: add super(message)

--- backend/src/utils/apiError.js
+++ backend/src/utils/apiError.js
@@ -12,6 +12,7 @@ class ApiError extends Error {
     errors = [],
     stack = ""
   ) {
+    super(message)
     this.statusCode = statusCode;
     this.message = message;
     this.errors = errors;
