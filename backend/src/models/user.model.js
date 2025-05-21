2d47ef721a1d1f19d435dd397863046354a986b4
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-05-21 12:10:06 +0530

    fix: fix typo and incorrect condition in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -8,7 +8,6 @@ import {
 } from "../constants.js";
 import crypto from "crypto";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -46,7 +45,7 @@ const userSchema = new Schema(
       default: UserRolesEnum.ADMIN,
       required: true,
     },
-    isEmailVarified: {
+    isEmailVerified: {
       type: Boolean,
       default: false,
     },
@@ -58,16 +57,16 @@ const userSchema = new Schema(
     refreshToken: {
       type: String,
     },
-    forgetPasswordToken: {
+    forgotPasswordToken: {
       type: String,
     },
-    forgetPasswordExpiry: {
+    forgotPasswordExpiry: {
       type: Date,
     },
-    emailVarificationToken: {
+    emailVerificationToken: {
       type: String,
     },
-    emailVarificationExpiry: {
+    emailVerificationExpiry: {
       type: Date,
     },
   },
@@ -75,36 +74,13 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) return next();
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      email: this.email,
-      username: this.username,
-      role: this.role,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-  );
-};
-
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-  );
 };
 
 userSchema.methods.generateTemporaryToken = function () {
