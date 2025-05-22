86f473ca5730f4ad387cc6e13fb9c18357d4c174
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-05-23 01:29:18 +0530

    refactor: remove route from app

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,9 +30,7 @@ import { errorHandler } from "./middlewares/error.middleware.js";
 import healthcheckRouter from "./routes/healthcheck.route.js";
 import userRouter from "./routes/user.route.js";
 import projectRouter from "./routes/project.route.js";
-import projectMemberRouter from "./routes/projectmember.route.js";
 import taskRouter from "./routes/task.route.js";
-import subtaskRouter from "./routes/subtask.route.js";
 import noteRouter from "./routes/note.route.js";
 
 // healthcheck route
@@ -41,9 +39,7 @@ app.use("/api/v1/healthcheck", healthcheckRouter);
 // application route
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/projects", projectRouter);
-app.use("/api/v1/team-member", projectMemberRouter);
 app.use("/api/v1/tasks", taskRouter);
-app.use("/api/v1/subtasks", subtaskRouter);
 app.use("/api/v1/notes", noteRouter);
 
 app.use(errorHandler);
