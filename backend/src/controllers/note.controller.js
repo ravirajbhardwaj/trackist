b3ffa22c947c99b38f5ef9c99473377a7d565909
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-05-24 22:30:29 +0530

    feat: add add note validator, route & controller

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -1,23 +1,102 @@
+import { Project } from "../models/project.model.js";
+import { Note } from "../models/note.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
 
 const getNotes = asyncHandler(async (req, res) => {
   // get all notes
 });
 
 const getNoteById = asyncHandler(async (req, res) => {
-  // get note by id
+  const { projectId, noteId } = req.params;
+
+  if (!projectId && !noteId) {
+    throw new ApiError(400, "Project & Note id is required");
+  }
+
+  const note = await Note.findById(noteId).populate(
+    "createdBy",
+    "username role avatar"
+  );
+
+  if (!note) {
+    throw new ApiError(404, "Note not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "Note fetch successfully", note));
 });
 
 const createNote = asyncHandler(async (req, res) => {
-  // create note
+  const { projectId } = req.params;
+  const { content } = req.body;
+
+  if (!projectId && !content) {
+    throw new ApiError(400, "Project id and content is required");
+  }
+
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+  const note = await Note.create({
+    project: mongoose.Types.ObjectId(projectId),
+    content,
+    createdBy: mongoose.Types.ObjectId(req.user._id),
+  });
+
+  const populatedNote = await Note.findById(note._id).populate(
+    "createBy",
+    "username role avatar"
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "Note created successfully", populatedNote));
 });
 
 const updateNote = asyncHandler(async (req, res) => {
-  // update note
+  const { projectId, noteId } = req.params;
+  const { content } = req.body;
+
+  if (!projectId && !noteId && !content) {
+    throw new ApiError(400, "Project & Note id and content is required");
+  }
+
+  const existedNote = await Note.findById(noteId);
+
+  if (!existedNote) {
+    throw new ApiError(404, "Note does not exist");
+  }
+  const note = await Note.findByIdAndUpdate(
+    noteId,
+    { content },
+    { new: true }
+  ).populate("createBy", "username role avatar");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "Note updated successfully", note));
 });
 
 const deleteNote = asyncHandler(async (req, res) => {
-  // delete note
+  const { projectId, noteId } = req.params;
+
+  if (!projectId && !noteId) {
+    throw new ApiError(400, "Project& Note id is required");
+  }
+
+  const deletedNote = await Note.findByIdAndDelete(noteId);
+
+  if (!deletedNote) {
+    throw new ApiError(404, "Note does not exist");
+  }
+
+  return res.status(200).json(200, "Note delete successfully", deletedNote);
 });
 
 export { createNote, deleteNote, getNoteById, getNotes, updateNote };
--- backend/src/routes/note.route.js
+++ backend/src/routes/note.route.js
@@ -1,8 +1,24 @@
 import { Router } from "express";
-import {} from "../controllers/note.controller.js";
+import {
+  createNote,
+  deleteNote,
+  getNoteById,
+  getNotes,
+  updateNote,
+} from "../controllers/note.controller.js";
+import { verifyAccessToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/");
+router
+  .route("/:projectId")
+  .get(verifyAccessToken, getNotes)
+  .post(verifyAccessToken, createNote);
+
+router
+  .route("/projectId/n/:noteId")
+  .get(verifyAccessToken, getNoteById)
+  .put(verifyAccessToken, updateNote)
+  .delete(verifyAccessToken, deleteNote);
 
 export default router;
