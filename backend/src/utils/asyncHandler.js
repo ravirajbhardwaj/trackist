959b9335600a938ccf708a4d4c7c6571596970cf
RaviRajBhardwaj <exploringwith.ravirajbhar@gmail.com>
2026-04-09 01:59:04 +0530

    refactor: migrate backend to new stack and restructure into src
    - replace Express with Hono
    - replace Mongoose with Drizzle ORM
    - migrate MongoDB to Neon PostgreSQL
    - switch Nodemailer/Mailtrap to Resend
    - replace express-validator with Zod
    - reorganize codebase into src directory

--- backend/src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = requestHandler => {
-  return function (req, res, next) {
-    Promise.resolve(requestHandler(req, res, next)).catch(error => next(error));
-  };
-};
-
-export { asyncHandler };
--- backend/src/validators/note.validator.js
+++
@@ -1,5 +0,0 @@
-import { body } from "express-validator";
-
-export const noteCreateAndUpdateValidator = () => {
-  return [body("content").trim().notEmpty().withMessage("Content is required")];
-};
